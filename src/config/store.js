7d9c17c7420cd367623d3cc0d0336a86b199db7e
Patryk Ziemkowski <pziemkowski@apptension.com>
2020-09-11 09:48:59 +0200

    config/configure-redux-saga

--- src/config/store.js
+++ src/config/store.js
@@ -1,7 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 
+import rootSaga from "./rootSaga";
 import rootReducer from "./rootReducer";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware().concat([sagaMiddleware]),
 });
+
+sagaMiddleware.run(rootSaga);
